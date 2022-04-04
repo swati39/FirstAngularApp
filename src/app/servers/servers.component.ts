07e5daa7bc03d8370ed4a0dd720ed1bbdf5af36b
Swati Inje <swatiinje201@gmail.com>
2022-04-04 18:59:58 +0530

    index and array of timestamp

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -38,6 +38,7 @@ export class ServersComponent implements OnInit {
   preview() {
     this.paraView = !this.paraView;
     this.log.push(this.log.length + 1);
+    this.log.push(new Date());
     console.log(this.count++);
   }
 }
